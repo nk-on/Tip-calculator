dcd146531fe384009d03e9a2123f7e35bb736d75
nk-on <johndoe@example.com>
2024-05-28 22:16:27 +0400

    Preventing user from entering empty bill amount

--- src/script.js
+++ src/script.js
@@ -5,6 +5,10 @@ function calculateTip(e) {
   e.preventDefault();
   const tipPercentage = Number((e.target.textContent).slice(0, -1));
   const billAmount = billAmountInput.value;
+  if(billAmount.length  === 0){
+    alert('enter bill amount')
+    return;
+  };
   const tipAmount = (billAmount*tipPercentage)/100;
   tipAmountResult.textContent = tipAmount;
 }

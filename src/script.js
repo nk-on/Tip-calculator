416fded6393e4161ca8e52ed6576d79456597952
nk-on <johndoe@example.com>
2024-08-25 14:08:44 +0400

    Feat:Displaying calculation results

--- src/script.js
+++ src/script.js
@@ -2,10 +2,14 @@ const billInput = document.querySelector('#bill-input');
 const tipPercentageButtons = document.querySelectorAll('.tip-percentage');
 const customInput = document.querySelector('#custom-input');
 const amountOfPeoplInput = document.querySelector('#number-of-people-input');
-const timAmountResultContainer = document.querySelector('.tip-amount-result');
+const tipAmountResultContainer = document.querySelector('.tip-amount-result');
 const totalAmountContainer = document.querySelector('.total-amount-result');
 //Application should alert user with error if they don;t enter anything or dont enter number
 //application should calculate tip amount and total amount when they click on tip percatage button
+function displayResults(tipAmount,totalAmount){
+    tipAmountResultContainer.textContent = `$ ${String(tipAmount)}`;
+    totalAmountContainer.textContent = `$ ${String(totalAmount)}`;
+}
 function calculate(e) {
   e.preventDefault();
   const [billAmount, amountOfPeople] = [
@@ -17,6 +21,7 @@ function calculate(e) {
     : this.dataset.percentage;
   const tipAmount = (billAmount * percentageAmount) / 100;
   const totalAmount = (tipAmount * amountOfPeople).toFixed(2);
+  displayResults(tipAmount,totalAmount);
 }
 tipPercentageButtons.forEach((button) => {
   button.addEventListener('click', calculate);

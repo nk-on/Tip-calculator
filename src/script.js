92d7f7310efc88e9910e3590ef674a8d7733362c
nk-on <johndoe@example.com>
2024-08-25 14:09:27 +0400

    fixing typos

--- src/script.js
+++ src/script.js
@@ -1,7 +1,7 @@
 const billInput = document.querySelector('#bill-input');
 const tipPercentageButtons = document.querySelectorAll('.tip-percentage');
 const customInput = document.querySelector('#custom-input');
-const amountOfPeoplInput = document.querySelector('#number-of-people-input');
+const amountOfPeopleInput = document.querySelector('#number-of-people-input');
 const tipAmountResultContainer = document.querySelector('.tip-amount-result');
 const totalAmountContainer = document.querySelector('.total-amount-result');
 //Application should alert user with error if they don;t enter anything or dont enter number
@@ -14,7 +14,7 @@ function calculate(e) {
   e.preventDefault();
   const [billAmount, amountOfPeople] = [
     Number(billInput.value),
-    Number(amountOfPeoplInput.value),
+    Number(amountOfPeopleInput.value),
   ];
   const percentageAmount = isNaN(this.dataset.percentage)
     ? this.value

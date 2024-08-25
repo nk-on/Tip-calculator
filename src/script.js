64d2150d22e8ce17985f042c1b1b8b3fe7e47b10
nk-on <johndoe@example.com>
2024-08-25 13:36:20 +0400

    Declaring variables

---
+++ src/script.js
@@ -0,0 +1,6 @@
+const billInput = document.querySelector('#bill-input');
+const tipPercentageButtons = document.querySelectorAll('.tip-percentage');
+const customInput = document.querySelector('#custom-input');
+const amountOfPeoplInput = document.querySelector('#number-of-people-input');
+const timAmountResultContainer = document.querySelector('.tip-amount-result');
+const totalAmountContainer = document.querySelector('.total-amount-result"');

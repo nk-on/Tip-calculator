124320067a89571c8783aaa603c9eeeaf0830574
nk-on <johndoe@example.com>
2024-08-25 21:13:11 +0400

    Moving initial tip amount and inital total amount variables to the beginning of code

--- src/script.js
+++ src/script.js
@@ -4,6 +4,10 @@ const customInput = document.querySelector('#custom-input');
 const amountOfPeopleInput = document.querySelector('#number-of-people-input');
 const tipAmountResultContainer = document.querySelector('.tip-amount-result');
 const totalAmountContainer = document.querySelector('.total-amount-result');
+/*Setting initial tip amount and total amount which will be data from local storage 
+otherwise empty string*/
+const initialTipAmount = localStorage.getItem('tipAmount') || '';
+const initialTotalAmount = localStorage.getItem('totalAmount') || '';
 function saveToLocalStorage(tipAmount, totalAmount){
   localStorage.setItem('tipAmount',tipAmount);
   localStorage.setItem('totalAmount',totalAmount);
@@ -30,7 +34,7 @@ function calculate(e) {
     Number(amountOfPeopleInput.value),
   ];
   validateInput(billAmount, amountOfPeople);
-  /*Setting percentage amount if user clicked on percentage button setting percentage amount as 
+  /*Setting percentage amount -  if user clicked on percentage button,  setting percentage amount as 
    data set property of this button otherwise number which was entered by user in custom form
   */
   const percentageAmount = isNaN(this.dataset.percentage)
@@ -46,8 +50,4 @@ tipPercentageButtons.forEach((button) => {
   button.addEventListener('click', calculate);
 });
 customInput.addEventListener('input', calculate);
-/*Setting initial tip amount and total amount which will be data from local storage 
-otherwise empty string*/
-const initialTipAmount = localStorage.getItem('tipAmount') || '';
-const initialTotalAmount = localStorage.getItem('totalAmount') || '';
 displayResults(initialTipAmount,initialTotalAmount);

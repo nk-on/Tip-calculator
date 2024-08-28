48c1fcd22e5889f4e645bded66e183bd4703fcb0
nk-on <johndoe@example.com>
2024-08-28 14:53:28 +0400

    Feat:calling API both with tipAmount and totalAmount variables and returning response

--- src/currencyConversion.js
+++ src/currencyConversion.js
@@ -1,7 +1,13 @@
-import { billInput, customInput, displayResults,initialTipAmount,initialTotalAmount } from "./script.js";
+import {
+  billInput,
+  customInput,
+  displayResults,
+  initialTipAmount,
+  initialTotalAmount,
+} from './script.js';
 const host = 'api.frankfurter.app';
-const API = `https://${host}/latest?from=USD`;
 const currencyOptions = document.querySelector('#currency-selector');
+let currentCurrency = localStorage.getItem('currency') || 'USD';
 //App should be able to convert exsisting tip Amount and total amount to different currency
 //if there is no default currency app should be just able to add differenct currency signs
 function changeSign(currency) {
@@ -12,19 +18,29 @@ function changeSign(currency) {
     ['CHF', 'Fr.'], // Swiss Franc
   ]);
   const currencySign = currencySymbols.get(currency);
-  localStorage.setItem("currencySign",currencySign);
-  displayResults(initialTipAmount,initialTotalAmount,currencySign);
+  localStorage.setItem('currencySign', currencySign);
+  displayResults(initialTipAmount, initialTotalAmount, currencySign);
   billInput.placeholder = customInput.placeholder = currencySign;
-};
-function currencyConversion(data){
-  console.log(data);
-};
-async function getCurrencyRates() {
-  const res = await fetch(API);
-  const data = await res.json();
-  return data;
+}
+async function getCurrencyRates(amount, currencyFrom, targetCurrency) {
+  const { initialTipAmount, initialTotalAmount } = amount;
+  let totalAmountResponse;
+  let tipAmountResponse;
+  const tipAmountData = fetch(
+    `https://${host}/latest?amount=${initialTipAmount}&from=${currentCurrency}&to${targetCurrency}`
+  ).then((res)=> res.json());
+  const totalAmountData = fetch(
+    `https://${host}/latest?amount=${initialTotalAmount}&from=${currentCurrency}&to${targetCurrency}`
+  ).then((res)=>res.json());
+  [totalAmountResponse,tipAmountResponse] = await Promise.all([tipAmountData,totalAmountData]);
+  console.log(totalAmountResponse,tipAmountResponse)
 }
 currencyOptions.addEventListener('change', (e) => {
   changeSign(e.target.value);
-  currencyConversion(getCurrencyRates());
+  getCurrencyRates(
+    { initialTipAmount, initialTotalAmount },
+    currentCurrency,
+    e.target.value
+  );
+  localStorage.setItem('currency', e.target.value);
 });

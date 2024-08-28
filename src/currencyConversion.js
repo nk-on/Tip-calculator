7cac021c4bb3942d2791ec67a6d7a78809bbde24
nk-on <johndoe@example.com>
2024-08-28 09:41:48 +0400

    Changing base currency to USD in API

--- src/currencyConversion.js
+++ src/currencyConversion.js
@@ -1,6 +1,6 @@
 import { billInput, customInput, displayResults,initialTipAmount,initialTotalAmount } from "./script.js";
 const host = 'api.frankfurter.app';
-const API = `https://${host}/latest?`;
+const API = `https://${host}/latest?from=USD`;
 const currencyOptions = document.querySelector('#currency-selector');
 //App should be able to convert exsisting tip Amount and total amount to different currency
 //if there is no default currency app should be just able to add differenct currency signs
@@ -16,6 +16,9 @@ function changeSign(currency) {
   displayResults(initialTipAmount,initialTotalAmount,currencySign);
   billInput.placeholder = customInput.placeholder = currencySign;
 };
+function currencyConversion(data){
+  console.log(data);
+};
 async function getCurrencyRates() {
   const res = await fetch(API);
   const data = await res.json();
@@ -23,5 +26,5 @@ async function getCurrencyRates() {
 }
 currencyOptions.addEventListener('change', (e) => {
   changeSign(e.target.value);
+  currencyConversion(getCurrencyRates());
 });
-console.log(getCurrencyRates());
